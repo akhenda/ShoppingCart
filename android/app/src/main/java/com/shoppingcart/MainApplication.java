9c9a8204596dfcb8d1d5dea08da34bc7d03a27f1
andela-akhenda <joseph.akhenda@andela.com>
2017-12-22 01:24:48 +0300

    (chore) configure release build options

--- android/app/src/main/java/com/shoppingcart/MainApplication.java
+++ android/app/src/main/java/com/shoppingcart/MainApplication.java
@@ -3,6 +3,7 @@ package com.shoppingcart;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseAuthPackage(),
